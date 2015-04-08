2c63f882ffa0b8e5e001ee934c5dd6f8bac1b138
mrweix <konstantinweixelbaum@gmail.com>
2015-04-08 00:00:01 -0500

    Clean up

--- GoEuroTest/src/GoEuroTest.java
+++ GoEuroTest/src/GoEuroTest.java
@@ -2,9 +2,6 @@
 /**
 * @author Konstantin Weixelbaum
 */
-
-//Importing used libraries
-
 //native java libraries
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -13,41 +10,32 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 //Google gson to parse json files
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-
- 
 public class GoEuroTest {
 
 	public static void main(String[] args) throws Exception 
 	{
-		
 		//If no argument is entered throw an exception and inform the user to enter one argument. An example is given 
 		if(args.length==0)
 		{
 			throw new Exception("No Argument entered! Please enter one argument as in java -jar GoEuroTest.jar \"Berlin\"");
-
 		}
 		//else execute the function turnJsonInfoToCsv
 		else
 		{
-			
 				for (int i = 0; i < args.length; i++)
 				{
-
 					String QueryString=args[i];
 					//call to the central function turnJsonInfoToCsv with the QueryStrings. The boolean true shows that the Querystring hasn't been changed since the Users input.
-					turnJsonInfoToCsv(QueryString,true);
-					
+					turnJsonInfoToCsv(QueryString,true);	
 				}
 		}
 	}
-	
 	
 	//the function to turn JSON into CSV
 	public static void turnJsonInfoToCsv(String Citystring, boolean exact)  throws Exception {
@@ -66,8 +54,6 @@ public class GoEuroTest {
 		    JsonParser jsonparser = new JsonParser(); //from gson
 		    JsonElement root = jsonparser.parse(new InputStreamReader((InputStream) request.getContent())); 
 		    JsonArray rootArray = root.getAsJsonArray(); //may be an array, may be an object. 
-			
-		    
 		    
 	    	//In case there are no search results try so long to shorten the Citystring until a result is found. 
 		    if(rootArray.size()==0)
@@ -89,13 +75,11 @@ public class GoEuroTest {
 	    		//if the Citystring was shortened and a result was found display the possibilities to the user. No csv file is created.
 		    	if(!exact)
 		    	{
-		    		
 		    		String ExceptionString="There was nothing returned for your search. Did you maybe mean ";
 		    		 for (int i = 0; i < rootArray.size(); i++) 
 		     	    {
 		     	    	JsonObject rootobj = rootArray.get(i).getAsJsonObject();
 		     	    	ExceptionString+="\r\n"+getKey(null,"name",rootobj);
-
 		     	    }
 		    		 throw new Exception(ExceptionString); 
 		    	}
@@ -115,24 +99,19 @@ public class GoEuroTest {
 		    	    parentKey[4]="geo_position";
 		    	    keyArray[0][5]="longitude";
 		    	    parentKey[5]="geo_position";
-		    	    
-		    	  
+
 		    	    //loop over all resulting Elements for the search item Citystring
 		    	    for (int i = 0; i < rootArray.size(); i++) 
 		    	    {
-		    	  
 		    	    	JsonObject rootobj = rootArray.get(i).getAsJsonObject();
-		    	
+		    	    	
 		    	    	//loop over all keys that we want to catch and write the result into an 2-dimensional Array.
-		    	       	
 		    	    	for(int j = 0; j < keyArray[0].length; j++)
 		    	    	{
 		    	       		//call to a function getKey which returns the content of a certain key if it exists
 		    	       		keyArray[i+1][j]=getKey(parentKey[j],keyArray[0][j],rootobj);
 		    	    	}
-		    	   
 		    	    }
-		    	    
 		    	    
 		    	    //create the CSV file from the 2-dimensional Array.
 		    	    //The file is named after the search query Citystring
@@ -140,8 +119,7 @@ public class GoEuroTest {
 		    	    //Success message. CSV file is only created if the query was successful.
 					System.out.println("Success! "+Citystring+".csv created");
 		    	}
-		    
-			    
+   
 		    }
 		}
 		catch (IOException e)
@@ -149,7 +127,6 @@ public class GoEuroTest {
 			System.out.println("Connection problem: ");
 			e.printStackTrace();
 		}
-	 
 	}
 	
 	private static String getKey(String parent,String keyname, JsonObject rootobj)
@@ -167,9 +144,7 @@ public class GoEuroTest {
 			JsonElement parentelement = rootobj.get(parent);
 	        JsonObject parentobj = parentelement.getAsJsonObject();
 	        key=checkIfKeynameExists(keyname,parentobj);
-			
 		}
-		
 		return key;
 	}
 	
@@ -188,10 +163,8 @@ public class GoEuroTest {
 			//return error message if keyname doesn't exist in this object.
 			key="Key "+keyname+" not found";
 		}
-		
 		return key;
 	}
-	
 	
 	//function to write the CSV file from the 2D Array with the native Java class BufferedWriter
 	private static void generateCsvFileFrom2DArray(String sFileName,String[][] array)throws IOException
@@ -216,6 +189,4 @@ public class GoEuroTest {
 	    br.write(sb.toString());
 	    br.close();
 	}
-	
 }
-
